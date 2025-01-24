3b6ca3148b1d8ac66149e4b4253c3e15b2e58b71
dongwonkim <kkum9408@gmail.com>
2025-01-25 04:14:44 +0900

    remove: redis options due to DI

--- type/redis.ts
+++ type/redis.ts
@@ -1,4 +1,2 @@
 import type { RedisClientOptions, RedisClientType, RedisModules } from 'redis';
-
-export type RedisConfig = { url: string };
 export type { RedisClientOptions, RedisClientType, RedisModules };
