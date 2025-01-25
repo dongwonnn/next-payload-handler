4a74426b9568a507ea995179613255a4d22bf316
dongwonkim <kkum9408@gmail.com>
2025-01-26 03:18:11 +0900

    refactor: improvements to extend the handler

--- src/CacheHandler.ts
+++ src/CacheHandler.ts
@@ -1,34 +1,40 @@
-import { RedisClientType } from 'redis';
-import type {
-  CacheHandlerCtxTags,
-  CacheHandlerKey,
-  CacheHandlerParametersGet,
-  CacheHandlerParametersSet,
-  CacheHandlerValue,
-} from '../type';
+import { RedisHandler } from './handlers/redis-handler';
+import { Handler } from './interface/handler-interface';
+
+import type { RedisClientType, CacheHandlerParametersGet, CacheHandlerParametersSet, CacheHandlerValue } from './type';
+
+type HandlerType = 'redis';
+type ClientType = RedisClientType;
+type HandlerInstanceType = InstanceType<typeof RedisHandler>;
 
 export class CacheHandler {
-  static #handler: RedisClientType | null = null;
+  static #handler: Handler | null = null;
 
-  static async initializeHandler(initialize: () => Promise<RedisClientType>): Promise<void> {
-    if (!CacheHandler.#handler) {
-      CacheHandler.#handler = await initialize();
+  static readonly #handlerCreators: Record<HandlerType, (client: ClientType) => HandlerInstanceType> = {
+    redis: (client) => new RedisHandler(client),
+  };
+
+  static async initializeHandler(type: HandlerType, initialize: () => Promise<ClientType>): Promise<void> {
+    if (CacheHandler.#handler) {
+      console.log('Handler is already initialized.');
+      return;
     }
-  }
 
-  #generateRedisKey(key: CacheHandlerKey, tags?: CacheHandlerCtxTags): string {
-    return tags && tags.length > 0 ? tags.join(',') : String(key);
+    const createHandler = CacheHandler.#handlerCreators[type];
+    if (!createHandler) {
+      throw new Error(`Unsupported client type: ${type}`);
+    }
+
+    const client = await initialize();
+    CacheHandler.#handler = createHandler(client);
+    console.log(`Handler initialized for type: ${type}`);
   }
 
   async get(key: CacheHandlerParametersGet[0], ctx: CacheHandlerParametersGet[1]): Promise<CacheHandlerValue | null> {
-    const redisKey = this.#generateRedisKey(key, ctx.tags);
-    try {
-      const cachedValue = await CacheHandler.#handler?.get(redisKey);
-      return cachedValue ? JSON.parse(cachedValue) : null;
-    } catch (error) {
-      console.error('Error fetching from Redis:', error);
-      return null;
+    if (!CacheHandler.#handler) {
+      throw new Error('Handler is not initialized.');
     }
+    return CacheHandler.#handler.get(key, ctx);
   }
 
   async set(
@@ -36,17 +42,9 @@ export class CacheHandler {
     value: CacheHandlerParametersSet[1],
     ctx: CacheHandlerParametersSet[2],
   ): Promise<void> {
-    const redisKey = this.#generateRedisKey(key, ctx.tags);
-    const cacheData = {
-      value,
-      lastModified: Date.now(),
-      tags: ctx.tags,
-    };
-
-    try {
-      await CacheHandler.#handler?.set(redisKey, JSON.stringify(cacheData));
-    } catch (error) {
-      console.error('Error setting value in Redis:', error);
+    if (!CacheHandler.#handler) {
+      throw new Error('Handler is not initialized.');
     }
+    await CacheHandler.#handler.set(key, value, ctx);
   }
 }
