73785d4a9a7df6b05cdcd41830038f77e6d08053
dongwonkim <kkum9408@gmail.com>
2025-01-26 03:16:19 +0900

    feat: separate redis handler

---
+++ src/handlers/redis-handler.ts
@@ -0,0 +1,51 @@
+import { Handler } from '../interface/handler-interface';
+import type {
+  RedisClientType,
+  CacheHandlerCtxTags,
+  CacheHandlerKey,
+  CacheHandlerParametersGet,
+  CacheHandlerParametersSet,
+  CacheHandlerValue,
+} from '../type';
+
+export class RedisHandler implements Handler {
+  private client: RedisClientType;
+
+  constructor(client: RedisClientType) {
+    this.client = client;
+  }
+
+  getCustomKey(key: CacheHandlerKey, tags?: CacheHandlerCtxTags): string {
+    return tags && tags.length > 0 ? tags.join(',') : String(key);
+  }
+
+  async get(key: CacheHandlerParametersGet[0], ctx: CacheHandlerParametersGet[1]): Promise<CacheHandlerValue | null> {
+    const redisKey = this.getCustomKey(key, ctx.tags);
+    try {
+      const cachedValue = await this.client.get(redisKey);
+      return cachedValue ? JSON.parse(cachedValue) : null;
+    } catch (error) {
+      console.error('Error fetching from Redis:', error);
+      return null;
+    }
+  }
+
+  async set(
+    key: CacheHandlerParametersSet[0],
+    value: CacheHandlerParametersSet[1],
+    ctx: CacheHandlerParametersSet[2],
+  ): Promise<void> {
+    const redisKey = this.getCustomKey(key, ctx.tags);
+    const cacheData = {
+      value,
+      lastModified: Date.now(),
+      tags: ctx.tags,
+    };
+
+    try {
+      await this.client.set(redisKey, JSON.stringify(cacheData));
+    } catch (error) {
+      console.error('Error setting value in Redis:', error);
+    }
+  }
+}
