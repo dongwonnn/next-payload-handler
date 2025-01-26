8650145bbe88e9a61bc60d3dc6dbc24e0720da87
dongwonkim <kkum9408@gmail.com>
2025-01-27 03:47:06 +0900

    feat: add CacheHandler extend type

--- src/type/cacheHandler.ts
+++ src/type/cacheHandler.ts
@@ -1,7 +1,18 @@
 import type { CacheHandler, CacheHandlerValue } from 'next/dist/server/lib/incremental-cache';
+import type { Bucket as GCSBucketType, RedisClientType } from './index';
+import type { GCSHandler, RedisHandler } from '../handlers';
 
 export type CacheHandlerParametersGet = Parameters<CacheHandler['get']>;
 export type CacheHandlerParametersSet = Parameters<CacheHandler['set']>;
 export type CacheHandlerCtxTags = CacheHandlerParametersGet[1]['tags'] | CacheHandlerParametersSet[2]['tags'];
 export type CacheHandlerKey = CacheHandlerParametersGet[0] | CacheHandlerParametersSet[0];
-export type { CacheHandlerValue };
+
+// TODO: fix generic type
+export type HandlerType = 'redis' | 'gcs';
+export type ClientType = RedisClientType | GCSBucketType;
+export type HandlerInstanceType = InstanceType<typeof RedisHandler> | InstanceType<typeof GCSHandler>;
+export type optionsType = {
+  bucketPrefix: string;
+};
+
+export type { CacheHandlerValue, GCSHandler, RedisHandler };
